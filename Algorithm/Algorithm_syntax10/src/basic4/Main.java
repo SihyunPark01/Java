0e90e0b949f6d66f4d29de260a8461cc2078da7d
marongrong <maro0226@naver.com>
2022-02-04 23:48:08 +0900

    CodingTest

---
+++ Algorithm/Algorithm_syntax10/src/basic4/Main.java
@@ -0,0 +1,25 @@
+package basic4;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int inputNum = 25;
+		int bin[] = new int[100];
+		
+		int i = 0;
+		int mok = inputNum;
+		
+		while (mok > 0) {
+			bin[i] = mok % 2;
+			mok /= 2;
+			i++;
+		}
+		
+		i--;
+		for (; i>=0; i--) {
+			System.out.print(bin[i]);
+		}
+	}
+
+}
